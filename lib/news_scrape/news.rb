3ad397a40e18977b124a06bc7e90992ddc189703
Alvinluipo <lualvin1234@gmail.com>
2016-01-17 16:19:57 -0500

    second commit

--- lib/news_scrape/news.rb
+++ lib/news_scrape/news.rb
@@ -2,33 +2,26 @@ class NewsScrape::News
 
   attr_accessor :title, :article_url, :author
 
+  @@stories = []
 
   def initialize(title=nil, author=nil)
-    @title=title
-    @author=author
+    @title = title
+    @author = author
   end
 
   def self.today 
-    self.scrape
+    self.scrape_all
   end
-
-  def self.scrape
-    stories=[]
-
-    stories<< self.scrape_all
-
-    stories
-  end 
 
   def self.scrape_all
     doc = Nokogiri::HTML(open("http://www.nytimes.com"))
-    story=self.new
-    story.title = doc.search("h2.story-heading")[1].text
-    story.author = doc.search("p.byline")[1].text
-    story.article_url = doc.search("h2.story-heading a").attr("href")
-    story
-  
-
- 
+    doc.search("div .span-ab-layout div .collection").each do |story|
+      s = self.new
+      s.title = doc.search("h2.story-heading").text
+      s.author = doc.search("p.byline").text
+      s.article_url = doc.search("h2.story-heading a").attr("href")
+      @@stories << s
+    end
   end
+
 end
